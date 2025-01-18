51217916a3d0232e76d3defa3a18d640d27b604d
CoderDojo西那須野 <131562680+CoderDojoNishinasuno@users.noreply.github.com>
2025-01-18 17:33:04 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,45 +8,15 @@ radio.onReceivedNumber(function (receivedNumber) {
             . . . . .
             `)
     } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -75) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+    	
     } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -60) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+    	
     } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -50) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+    	
     } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -40) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+    	
     } else {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+    	
     }
     basic.clearScreen()
 })
